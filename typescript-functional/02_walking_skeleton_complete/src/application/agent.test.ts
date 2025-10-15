5acb36b3c8724a33a0cd06cd66dbd81aef010bac
Gregor Riegler <g.riegler@tricentis.com>
2025-10-15 16:05:39 +0200

    GREEN: llm answers

--- typescript-functional/02_walking_skeleton_complete/src/application/agent.test.ts
+++ typescript-functional/02_walking_skeleton_complete/src/application/agent.test.ts
@@ -1,28 +1,27 @@
 import { describe, it, expect } from "vitest";
-import { agent, Display, Input } from "./agent";
+import { agent, Display, Input, LanguageModel, Message } from "./agent";
 
 describe("Agent", () => {
   let textOnDisplay = "";
 
-  it("displays message from user", () => {
-    agent(inputStub, displaySpy);
-    expect(textOnDisplay).toBe("User: Hello, Agent!");
-  });
-  
-  it("sends user input to llm and displays its answer", () => {
-    agent(inputStub, displaySpy, languageModel);
+  it("displays message from user, sends user input to llm and displays its answer", () => {
+    agent(inputStub, displaySpy, languageModelStub);
     expect(textOnDisplay).toBe(
       "User: Hello, Agent!\n" +
-      "Agent: Hi there! Yes, you are right!"
+      "Agent: Hi there! Yes, you are right!\n"
     );
   });
+
+  const languageModelStub: LanguageModel = (messages: Message[]) => {
+    return { role: "agent", content: "Hi there! Yes, you are right!" };
+  }
 
   const inputStub: Input = () => {
     return "Hello, Agent!"
   };
 
   const displaySpy: Display = (text: string) => {
-    textOnDisplay += text
+    textOnDisplay += text + "\n";
   };
 });
 
