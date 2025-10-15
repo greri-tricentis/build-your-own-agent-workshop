71e8b92a7a10bfc60b8ce7afd6584ba7abf28b1b
Gregor Riegler <g.riegler@tricentis.com>
2025-10-15 10:20:19 +0200

    java template

---
+++ java/template/agent-tests/src/test/java/com/workshop/agent/tests/AgentTests.java
@@ -0,0 +1,12 @@
+package com.workshop.agent.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class AgentTests {
+    @Test
+    public void test1() {
+        fail();
+    }
+}
