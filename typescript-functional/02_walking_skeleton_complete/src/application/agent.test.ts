d045921d65b42fdaa18bfc43be9d2ae31556fbc3
Gregor Riegler <g.riegler@tricentis.com>
2025-10-15 15:35:15 +0200

    GREEN input message is displayed

--- typescript-functional/02_walking_skeleton_complete/src/application/agent.test.ts
+++ typescript-functional/02_walking_skeleton_complete/src/application/agent.test.ts
@@ -1,10 +1,25 @@
 import { describe, it, expect } from "vitest";
 
 describe("Agent", () => {
-  const textOnDisplay = "User: Hello, Agent!";
+  let textOnDisplay = "";
+
   it("displays message from user", () => {
     const message = input();
     display(`User: ${message}`);
     expect(textOnDisplay).toBe("User: Hello, Agent!");
   });
+  
+  type Input = () => string;
+  type Display = (text: string) => void;
+  
+  const input: Input = () => {
+    return "Hello, Agent!"
+  };
+
+
+  const display: Display = (text: string) => {
+    textOnDisplay += text
+  };
 });
+
+
