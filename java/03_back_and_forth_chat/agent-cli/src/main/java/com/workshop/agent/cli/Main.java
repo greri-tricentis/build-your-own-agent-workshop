0934dad5cafa059e6e16e29194d8e34595087953
Gregor Riegler <g.riegler@tricentis.com>
2025-10-24 13:26:34 +0200

    java 03 cli

--- java/03_back_and_forth_chat/agent-cli/src/main/java/com/workshop/agent/cli/Main.java
+++ java/03_back_and_forth_chat/agent-cli/src/main/java/com/workshop/agent/cli/Main.java
@@ -1,7 +1,18 @@
 package com.workshop.agent.cli;
 
+import com.workshop.agent.application.*;
+import com.workshop.agent.infrastructure.ConsoleDisplay;
+import com.workshop.agent.infrastructure.ConsoleUserInput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        UserInput input = new ConsoleUserInput();
+        LanguageModel model = new RepeatingLanguageModel();
+        //LanguageModel model = new OllamaLanguageModel("http://localhost:11434", "gemma3:1b");
+        Display display = new ConsoleDisplay();
+
+        Agent agent = new Agent(input, model, display);
+
+        agent.run();
     }
 }
